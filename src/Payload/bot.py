823d75fae479953508c0cbf06ef9964dc305d9de
squishy <89873236+squishy9876@users.noreply.github.com>
2024-04-24 15:42:48 -0400

    Update bot.py

--- src/Payload/bot.py
+++ src/Payload/bot.py
@@ -11,8 +11,8 @@ from icmplib import ping as pig
 from scapy.layers.inet import UDP
     
 # IP AND PORT C2 ------------------->
-KRYPTONC2_ADDRESS  = "localhost"
-KRYPTONC2_PORT     = 5511
+SQUASHEDC2_ADDRESS  = "localhost"
+SQUASHEDC2_PORT     = 5511
 
 
 # Code -------------------------->
